8c9a9443e2881e5a9255bc4ba836356f497f5128
Ben Ransford <ben@ransford.org>
2013-02-27 10:12:38 -0800

    clean up install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 ln -s "$PWD/.emacs" "$HOME"
 ln -s "$PWD/.gitconfig" "$HOME"
-ln -s "$PWD/.mailcap" "$HOME"
 ln -s "$PWD/.screenrc" "$HOME"
 ln -s "$PWD/.vim" "$HOME"
 ln -s "$PWD/.vimrc" "$HOME"
 ln -s "$PWD/.zshenv" "$HOME"
-ln -s "$PWD/.hgrc" "$HOME"
