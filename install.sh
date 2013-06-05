d86d0cddbef5c1a2ba1d41eec4b121f4558d1762
Ben Ransford <ben@ransford.org>
2013-06-05 13:11:34 -0400

    install tmux.conf

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 ln -s "$PWD/.emacs" "$HOME"
 ln -s "$PWD/.gitconfig" "$HOME"
 ln -s "$PWD/.screenrc" "$HOME"
+ln -s "$PWD/.tmux.conf" "$HOME"
 ln -s "$PWD/.vim" "$HOME"
 ln -s "$PWD/.vimrc" "$HOME"
 ln -s "$PWD/.zshenv" "$HOME"
