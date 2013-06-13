3e8d45f1ae274e14f3d89bfc9c78657624d10fb6
Ben Ransford <ben@ransford.org>
2013-06-13 12:27:26 -0400

    redo install script

--- install.sh
+++ install.sh
@@ -1,10 +1,35 @@
-#!/bin/sh
-ln -s "$PWD/.emacs" "$HOME"
-ln -s "$PWD/.gitconfig" "$HOME"
-ln -s "$PWD/.screenrc" "$HOME"
-ln -s "$PWD/.tmux.conf" "$HOME"
-ln -s "$PWD/.vim" "$HOME"
-ln -s "$PWD/.vimrc" "$HOME"
-ln -s "$PWD/.zshenv" "$HOME"
-ln -s "$PWD/.subversion" "$HOME"
-ln -s "$PWD/.Xresources" "$HOME"
+#!/bin/bash
+WHEREAMI=`dirname "$0"`
+RC=`cd "$WHEREAMI" && pwd -P | sed -e "s,^$HOME/,,"`
+
+function dbg() {
+	echo "$1" 1>&2
+}
+
+cd "$HOME"
+for x in \
+	.emacs \
+	.gitconfig \
+	.screenrc \
+	.subversion \
+	.tmux.conf \
+	.vim \
+	.vimrc \
+	.Xresources \
+	.zshenv \
+	; do
+	if [ -L "$x" ]; then
+		LTARGET=`readlink "$x"`
+		INODE1=`ls -id "$LTARGET" | cut -d' ' -f1`
+		INODE2=`ls -id "$RC/$x" | cut -d' ' -f1`
+		if [ "$INODE1" = "$INODE2" ]; then
+			dbg "$x: Already symlinked; doing nothing"
+		fi
+	elif [ -f "$x" ]; then
+		dbg "$x: File exists; doing nothing"
+	fi
+	continue
+
+	# make a symlink
+	ln -s "$RC/$x" .
+done
