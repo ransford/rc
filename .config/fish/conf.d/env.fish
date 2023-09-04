509f28b8d194e10af05199259dc01fafaf5d2737
Ben Ransford <ben@ransford.org>
2023-09-04 12:24:33 -0700

    editor fallthrough

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,11 +1,3 @@
-set -g OS (uname)
-set -g ARCH (uname -m)
-set -gx PAGER less
-set -gx EDITOR vim
-
-set -gx LSCOLORS "CxfxcxdxBxegedabagacad"
-set -gx FZF_DEFAULT_COMMAND fd
-
 function path_prepend -a dir
 	if not test -d $dir
 		return 1
@@ -23,3 +15,17 @@ function path_append -a dir
 		set PATH $PATH $dir
 	end
 end
+
+set -g OS (uname)
+set -g ARCH (uname -m)
+set -gx PAGER less
+
+for editor in hx vim vi
+	if hash $editor 2>/dev/null
+		set -gx EDITOR $editor
+		break
+	end
+end
+
+set -gx LSCOLORS "CxfxcxdxBxegedabagacad"
+set -gx FZF_DEFAULT_COMMAND fd
