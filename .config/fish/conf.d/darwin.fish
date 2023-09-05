3f15e52b8609b35bf8976221b4be580c78fca016
Ben Ransford <ben@ransford.org>
2023-09-05 14:31:03 -0700

    fix source idiom

--- .config/fish/conf.d/darwin.fish
+++ .config/fish/conf.d/darwin.fish
@@ -2,7 +2,7 @@
 test (uname) = Darwin; or exit 0
 
 if status is-interactive
-	eval (/opt/homebrew/bin/brew shellenv)
+	/opt/homebrew/bin/brew shellenv | source
 
 	abbr --add locate 'mdfind -name'
 	abbr --add fin 'mdfind -onlyin . -name'
